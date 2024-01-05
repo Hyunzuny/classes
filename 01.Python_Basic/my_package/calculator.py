0fa4fc5dd4daee32e34369dabde4d6d8c3f524a4
HyunJun An <hyunzuny@gmail.com>
2024-01-05 14:25:28 +0900

    01.Python_Basic 폴더 생성

---
+++ 01.Python_Basic/my_package/calculator.py
@@ -0,0 +1,12 @@
+
+def plus(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
--- 01.Python_Basic/run.py
+++ 01.Python_Basic/run.py
@@ -1,9 +1,15 @@
 #run.py에서 my_module의 함수 사용.
-import my_module as mm
+import my_module as mm  #무조건 별칭으로 호출.
+from my_package import calculator as c
+print(c.plus(5,6))
+
+print("__name__:", __name__)
+
 txt = mm.greet("홍길동")
 print(txt)
 
 p = mm.Person("이순신", 30)
+
 print(p)
 
 txt2 = mm.greet(p.name)
